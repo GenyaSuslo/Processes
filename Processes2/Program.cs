65a07b52a865c98d023a010ccdd9ad7e3c292699
susloparov <alfard.janya@yandex.ru>
2024-12-01 20:31:33 +0300

    #error

--- Processes2/Program.cs
+++ Processes2/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-using System.Diagnostics;
+//using System.Diagnostics;
+
+namespace Processes2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process[] allProcesses = Process.GetProcesses();
+            for (int i = 0; i < allProcesses.Length; i++)
+            {
+                Console.WriteLine($"{allProcesses[i].Id}\t{allProcesses[i].ProcessName}"); ;
+            }
+        }
+
+    }
+
+}
